f0fb9f9fe4a6e4490033357e0217f66957ddcc42
Wind <136448822+WindLapTrinh@users.noreply.github.com>
2024-11-19 17:32:02 +0700

    update source

--- wind-lap-trinh/app-diem-danh/src/pages/login/Home.jsx
+++ wind-lap-trinh/app-diem-danh/src/pages/login/Home.jsx
@@ -1,75 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Page, Box, Button, Text, Icon } from "zmp-ui";
-import SliderLogin from "./Slider";
-import Infomation from "./Infomation";
-import Security from "./Security";
-import Feature from "./Feature";
-import usePhoneNumber from "../shared/hooks/usePhoneNumber";
-import Introduce from "./Introduce";
-import CustomHeader from "../shared/pages/CustomHeader";
-import "../../css/login/login.css";
+import { Box, Input, Button } from "zmp-ui";
+import "../../css/login/home.css"; 
 
-const Login = () => {
+const Home = () => {
   const navigate = useNavigate();
-  const {
-    phoneNumber,
-    loading: phoneLoading,
-    error: phoneError,
-    fetchPhoneNumber,
-  } = usePhoneNumber();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedData = localStorage.getItem("userData");
-    if (storedData) {
-      const { phoneNumber } = JSON.parse(storedData);
-      if (phoneNumber) {
-        navigate("/students", { state: { phoneNumber } });
-      }
-    }
-  }, [navigate]);
-
-  useEffect(() => {
-    if (isLoggedIn && phoneNumber) {
-      localStorage.setItem("userData", JSON.stringify({ phoneNumber }));
-      navigate("/students", { state: { phoneNumber } });
-      console.log("Phone number:", phoneNumber);
-    }
-  }, [isLoggedIn, phoneNumber, navigate]);
-
-  const handleLogin = async () => {
-    await fetchPhoneNumber(); 
-    if (!phoneError) {
-      setIsLoggedIn(true);
-    } else {
-      console.error("Error fetching phone number:", phoneError);
-    }
-  };
-
+  const handleOnClickLogin = () =>{
+    navigate("/homePage")
+  }
   return (
-    <div>
-      {!isLoggedIn && (
-        <Page className="page-login">
-          <CustomHeader title={"HoangAnh DigiContact"} showBackIcon={false}/>
-          <Box className="header-login">
-            <Introduce />
-            <SliderLogin />
-            <Feature />
-          </Box>
-          <Box className="body-login">
-            <Security />
-            <Infomation />
-            <Box mt={2}>
-              <Button className="btn-login" fullWidth onClick={handleLogin}>
-                Đã hiểu
-              </Button>
-            </Box>
-          </Box>
-        </Page>
-      )}
-    </div>
+    <Box className="login-page" p={4}>
+      <Box className="login-container" p={4}>
+        <Input
+          type="text"
+          placeholder="Username"
+          className="input-field"
+          clearable
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="input-field"
+          clearable
+        />
+        <Button type="primary" className="login-button" onClick={handleOnClickLogin}>
+          Login
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
-export default Login;
+export default Home;
